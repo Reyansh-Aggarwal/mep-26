e85c434614adad8a35618f241da03727420f66a8
Reyansh Aggarwal <87906999+Reyansh-Aggarwal@users.noreply.github.com>
2026-07-07 23:10:34 +0530

    update brochure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
     return (
         <div
             className={cn(
-                "relative w-full bg-black overflow-hidden",
+                "relative w-full bg-black overflow-hidden select-none",
                 "flex flex-col items-center justify-center pt-24 pb-12 z-10"
             )}>
 
